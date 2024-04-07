3d4a5a3e824fc34f08ac64f743b77010519bc4f6
ZAKA-6X <89934286+ZAKA-6X@users.noreply.github.com>
2024-04-07 15:16:17 +0000

    upgrade code

--- pattern.py
+++ pattern.py
@@ -1,55 +1,41 @@
 # Pattern: Increasing Staircase
-n = 7
-for item in range(1, n+1):
-    line = '*' * item
-    print(line)
-
-print('\n')
+n = 6
+for i in range(1,n+1):
+    stars = i * '*'
+    print(stars)
 
 # Pattern: Decreasing Staircase
-n = 7
-for item in range(n, 0, -1):
-    line = '*' * item
-    print(line)
-
-print('\n')
+n = 6
+for i in range(n, 0, -1):
+    stars = i * '*'
+    print(stars)
 
 # Pattern: Right-aligned Staircase
-n = 7
-for item in range(n, 0, -1):
-    spaces = ' ' * (n - item)
-    stars = '*' * item
-    line = spaces + stars
-    print(line)
-
-print('\n')
+n = 6
+for i in range(1,n+1):
+    spaces = (n - i) * ' '
+    stars = i * '*'
+    print(spaces + stars)
 
 # Pattern: Left-aligned Staircase
-n = 7
-for item in range(n, 0, -1):
-    spaces = ' ' * item
-    stars = '*' * (n - item)
-    line = spaces + stars
-    print(line)
-
-print('\n')
+n = 6
+for i in range(1,n):
+    spaces = (i) * ' '
+    stars = (n - i) * '*'
+    print(spaces + stars)
 
 # Pattern: Diamond
-n = 7
-print(' ' * (n-1) + '*')
-for item in range(2, n+1):
-    spaces = ' ' * (n - item)
-    stars = '*' * (2 * item - 1)
-    line = spaces + stars
-    print(line)
-
-print('\n')
+n = 11
+head = (n)*' ' + '*'
+print(head)
+for i in range(1,n+1):
+    spaces = ' ' * (n-i) 
+    stars = '*' * i * 2 + '*'
+    print(spaces + stars)
 
 # Pattern: Reversed Diamond
-n = 7
-for item in range(n, 1, -1):
-    spaces = ' ' * (n - item)
-    stars = '*' * (2 * item - 1)
-    line = spaces + stars
-    print(line)
-print(' ' * (n-1) + '*')
+n = 11
+for i in range(1, n+1):
+    spaces = ' ' * (i) 
+    stars = '*' * (n-i) * 2 + '*'
+    print(spaces + stars)
